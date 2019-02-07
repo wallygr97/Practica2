2c869c457b51bc7dc496136bfbecf701ece0ecd3
wallygr97 <roodwally@gmail.com>
2019-02-07 19:37:46 -0400

    primer commit

---
+++ src/main/java/com/pucmm/edu/Estudiante.java
@@ -0,0 +1,52 @@
+package com.pucmm.edu;
+
+public class Estudiante {
+    private int matricula;
+    private String nombre;
+    private String apellido;
+    private String telefono;
+
+    public Estudiante(int matricula, String nombre, String apellido, String telefono) {
+        this.matricula = matricula;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+    }
+
+    public Estudiante() {
+
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+}
+
---
+++ src/main/java/com/pucmm/edu/Main.java
@@ -0,0 +1,7 @@
+package com.pucmm.edu;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
